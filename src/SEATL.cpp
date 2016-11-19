9258cf8784bd4ad5983014093d178f8add9a7f15
Rui Gonçalves <ruippeixotog@gmail.com>
2016-11-19 00:02:02 +0000

    Improve solution to SEATL

--- src/SEATL.cpp
+++ src/SEATL.cpp
@@ -1,91 +1,85 @@
 #include <algorithm>
 #include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <string>
-#include <utility>
 #include <vector>
 
-#define MAXN 1000000
-#define MAXM 1000
+#define MAXA 1000000
 
 using namespace std;
 
-typedef long long ll;
-typedef long double ld;
-
 vector<vector<int>> a;
 
-map<int, int> rowFreq[MAXN], colFreq[MAXM];
-int colMax[MAXM];
+vector<pair<int, int>> lines[MAXA + 1], cols[MAXA + 1];
 
-#define N 1000
-#define M 1000
+int countForK(int k) {
+  if(lines[k].empty()) return 0;
 
-void gen() {
-  cout << 1 << endl;
-  cout << N << " " << M << endl;
+  vector<int> bestLines, bestCols;
+  int bestLineCnt = 0, bestColCnt = 0;
 
-  int x[N][M];
-  for(int i = 0; i < N; i++) {
-    for(int j = 0; j < M; j++) {
-      x[i][j] = (i * N) + j;
+  for(auto line : lines[k]) {
+    if(line.second >= bestLineCnt) {
+      if(line.second > bestLineCnt) bestLines.clear();
+      bestLines.push_back(line.first);
+      bestLineCnt = line.second;
     }
-    random_shuffle(x[i], x[i] + M);
   }
 
-  for(int i = 0; i < N; i++) {
-    cout << x[i][0];
-    for(int j = 1; j < M; j++) cout << " " << x[i][j];
-    cout << endl;
+  for(auto col : cols[k]) {
+    if(col.second >= bestColCnt) {
+      if(col.second > bestColCnt) bestCols.clear();
+      bestCols.push_back(col.first);
+      bestColCnt = col.second;
+    }
   }
+
+  for(int i : bestLines) {
+    for(int j : bestCols) {
+      if(a[i][j] != k) return bestLineCnt + bestColCnt;
+    }
+  }
+  return bestLineCnt + bestColCnt - 1;
 }
 
 int main() {
-//  gen();
-//  return 0;
-
   int t; scanf("%d", &t);
   for(int tc = 1; tc <= t; tc++) {
     int n, m; scanf("%d %d", &n, &m);
-    int ni = max(n, m), mi = min(n, m);
 
-    a.resize(ni);
-    for(int i = 0; i < ni; i++) {
-      a[i].resize(mi);
-      for(int j = 0; j < mi; j++)
+    a.resize(n);
+    for(int i = 0; i < n; i++) {
+      a[i].resize(m);
+      for(int j = 0; j < m; j++) {
         scanf("%d", &a[i][j]);
-    }
-
-    for(int i = 0; i < ni; i++) rowFreq[i].clear();
-    for(int j = 0; j < mi; j++) colFreq[j].clear();
-
-    for(int i = 0; i < ni; i++) {
-      for(int j = 0; j < mi; j++) {
-        rowFreq[i][a[i][j]]++; colFreq[j][a[i][j]]++;
       }
     }
 
-    memset(colMax, 0, sizeof(colMax));
-    for(int i = 0; i < mi; i++) {
-      for(auto e: colFreq[i])
-        colMax[i] = max(colMax[i], e.second);
+    for(int k = 1; k <= MAXA; k++) {
+      lines[k].clear(); cols[k].clear();
+    }
+
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < m; j++) {
+        if (!lines[a[i][j]].empty() && lines[a[i][j]].back().first == i) {
+          lines[a[i][j]].back().second++;
+        } else {
+          lines[a[i][j]].push_back({i, 1});
+        }
+      }
+    }
+
+    for(int j = 0; j < m; j++) {
+      for(int i = 0; i < n; i++) {
+        if (!cols[a[i][j]].empty() && cols[a[i][j]].back().first == j) {
+          cols[a[i][j]].back().second++;
+        } else {
+          cols[a[i][j]].push_back({j, 1});
+        }
+      }
     }
 
     int best = 0;
-    for(int i = 0; i < ni; i++) {
-      cerr << i << endl;
-      for(int j = 0; j < mi; j++) {
-        int best2 = colMax[j];
-        for(auto e: rowFreq[i]) {
-          best2 = max(best2, e.second + colFreq[j][e.first] -
-              (e.first == a[i][j]));
-        }
-        best = max(best, best2);
-      }
+    for(int k = 1; k <= MAXA; k++) {
+      best = max(best, countForK(k));
     }
     printf("%d\n", best);
   }
